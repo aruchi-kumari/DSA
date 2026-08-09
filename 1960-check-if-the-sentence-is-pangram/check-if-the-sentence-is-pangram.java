ac18b876376bb7b83c33340ae61bac2d5564f4cc
Aruchi kumari <aruchikumari764@gmail.com>
2026-08-09 23:59:17 +0530

    Time: 1 ms (82.02%) | Memory: 43.1 MB (33.96%) - LeetSync

---
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        boolean[] seen = new boolean[26];
+
+        for (char ch : sentence.toCharArray()) {
+            seen[ch - 'a'] = true;
+        }
+
+        for (boolean value : seen) {
+            if (!value) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
